6f11d85c9f70417eee6234a5240cf956df18ac3e
fishsalted <10572677+fishsalted@users.noreply.github.com>
2019-04-06 19:15:36 +0800

    完成 AnswerGenerator class 的测试

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,28 @@
 package tw.core.generator;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
+
+    @Test
+    public void testAnswerGenerator_generate_function() throws Exception {
+
+        RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
+
+        String randomIntResult = "2 5 7 3";
+        when(randomIntGenerator.generateNums(9, 4)).thenReturn(randomIntResult);
+
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+
+        assertEquals(answerGenerator.generate().toString(), randomIntResult);
+
+    }
 
 }
 
