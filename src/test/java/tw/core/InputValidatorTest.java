f4c2eca1abb8320a231c9b0c53ab1ef2a3802cb6
fishsalted <10572677+fishsalted@users.noreply.github.com>
2019-04-07 11:02:28 +0800

    完成 InputValidator class 的测试

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,22 @@
 package tw.core;
 
+import org.junit.Test;
+import tw.validator.InputValidator;
+
+import static org.junit.Assert.*;
+
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
  */
 public class InputValidatorTest {
+
+    @Test
+    public void testInputValidator_validate_function() {
+        InputValidator inputValidator = new InputValidator();
+
+        assertTrue(inputValidator.validate("1 2 3 4"));
+
+        assertFalse(inputValidator.validate("0 1 1 2"));
+    }
 
 }
