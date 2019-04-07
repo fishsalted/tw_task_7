67cefc98adfa415982226909112f2bcc99aeb661
fishsalted <10572677+fishsalted@users.noreply.github.com>
2019-04-07 11:08:10 +0800

    完成 RandomIntGenerator class 的测试

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,25 @@
 package tw.core;
 
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
+import static org.junit.Assert.*;
 
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
+
+    @Test
+    public void testRandomIntGenerator_generateNums_function() {
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+
+        try {
+            randomIntGenerator.generateNums(4,6);
+            fail("RandomIntGenerator not throw exception");
+        }catch(IllegalArgumentException exception) {
+            assertEquals(exception.getMessage(), "Can't ask for more numbers than are available");
+        }
+    }
 
 }
